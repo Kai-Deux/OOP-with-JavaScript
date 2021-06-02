48cb068d5c121bf0996a2444f77ebfeb2c5f1219
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2021-06-02 17:35:51 +0200

    commit on 'master'

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
+    // Instance properties
     this.firstName = firstName;
     this.birthYear = birthYear;
+
+    // Never do this
+    // this.calcAge = function () {
+    //     console.log(2037 - this.birthYear);
+    // };
 };
 
 const kai = new Person('Kai', 1993);
@@ -12,3 +18,10 @@ console.log(kai);
 // 2. Function is called, this = {}
 // 3. {} linked to prototype
 // 4. Function automatically return {}
+
+const usha = new Person('Usha', 1968);
+const gray = new Person('Gray', 2017);
+console.log(usha, gray);
+
+console.log(kai instanceof Person);
+
