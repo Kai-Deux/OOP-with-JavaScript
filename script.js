79d6e346f71eae59e687b77b3a4a8a2d3396dabb
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2021-06-05 22:57:02 +0200

    commit on 'master'

--- script.js
+++ script.js
@@ -314,28 +314,34 @@ class Account {
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
-        this.pin = pin;
-        this.movements = [];
+        // Protected Property
+        this._pin = pin;
+        this._movements = [];
         this.locale = navigator.language;
 
         console.log(`Thanks for opening an account, ${owner}`);
     }
 
     // Public Interface
+    getMovements() {
+        return true;
+}
+
+
     deposit(val) {
-        this.movements.push(val)
+        this._movements.push(val)
     }
 
     withdraw(val) {
         this.deposit(-val)
     }
 
-    approveLoan(val) {
+    _approveLoan(val) {
         return true;
     }
 
     requestLoan(val) {
-        if (this.approveLoan(val)) {
+        if (this._approveLoan(val)) {
             this.deposit(val);
         console.log('Loan Approved');
     }
@@ -345,13 +351,15 @@ class Account {
 const acc1 = new Account('Kai', 'ZAR', 1111);
 console.log(acc1);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
 
+ 
