080faded7caa8d3a44a49001b418feedef533e67
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2021-06-06 21:27:33 +0200

    commit on 'master'

--- script.js
+++ script.js
@@ -310,42 +310,64 @@ jay.introduce();
 jay.calcAge();
 */
 
+// 1. Public fields
+// 2.Private fields
+// 3.Public methods
+// 4.Private methods
+// (There is also the static version)
+
+
 class Account {
+    // 1. Public fields (Instances)
+    locale = navigator.language;
+    
+    // 2. Private fields
+    #movements = [];
+    #pin;
+
+     
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
         // Protected Property
-        this._pin = pin;
-        this._movements = [];
-        this.locale = navigator.language;
+        this.#pin = pin;
+        // this.#movements = [];
+        // this.locale = navigator.language;
 
         console.log(`Thanks for opening an account, ${owner}`);
     }
 
+    // 3. Public Methods
+
     // Public Interface
     getMovements() {
-        return true;
+        return this.#movements;
 }
 
 
     deposit(val) {
-        this._movements.push(val)
+        this.#movements.push(val)
     }
 
     withdraw(val) {
         this.deposit(-val)
     }
 
-    _approveLoan(val) {
-        return true;
-    }
-
     requestLoan(val) {
+        // if (this.#approveLoan(val)) {
         if (this._approveLoan(val)) {
             this.deposit(val);
         console.log('Loan Approved');
     }
-}
+    }
+
+    static helper() {
+        console.log('Helper');
+    }
+    // 4. Private Methods
+    _approveLoan(val) {
+        return true;
+    }
 }
 
 const acc1 = new Account('Kai', 'ZAR', 1111);
@@ -353,13 +375,16 @@ console.log(acc1);
 
 // acc1._movements.push(250);
 // acc1._movements.push(-140);
+// acc1.approveLoan(1000);
+
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
 console.log(acc1.getMovements());
-
 console.log(acc1);
-console.log(acc1.pin);
 
- 
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
+
+Account.helper();
